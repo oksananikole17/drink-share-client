b946e8375cd5a0d9af3b98999fe3a44a0e132fa5
oksananikole17 <oksananikole17@gmail.com>
2022-05-27 14:12:44 -0500

    deploy link

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://git.heroku.com/calm-depths-04533.git',
+  production: 'https://calm-depths-04533.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
